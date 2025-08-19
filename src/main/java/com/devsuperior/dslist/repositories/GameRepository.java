8a1b3eef5b870fdff1b7a0fa934a9fab04ab628f
fabiotcm <fabiotarcisioccpa@gmail.com>
2025-08-19 18:46:45 -0300

    Service e Controller

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,4 +1,7 @@
 package com.devsuperior.dslist.repositories;
 
-public interface GameRepository {
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
 }
---
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.service;
+
+
+import com.devsuperior.dslist.DTO.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<Game> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result;
+
+    }
+}
